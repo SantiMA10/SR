f4246e334039c4e9c73db69217dec0d31e4ff680
Santiago Martin <santiagomartinagra@gmail.com>
2016-11-20 12:15:13 +0100

    ejercicio 5.4

--- Sesion5/Actividad-5.4/Actividad-5.4.ino
+++ Sesion5/Actividad-5.4/Actividad-5.4.ino
@@ -1,183 +1,167 @@
 #include <Servo.h>
 
 #define boton_pin 13 // pin digital para el botón
-#define Y_pinY A0 // pin analógico para leer eje X
-#define X_pinX A1 // pin analógico para leer eje X
+#define X_pin A0 // pin analógico para leer eje X
+#define Y_pin A1 // pin analógico para leer eje Y
 
-#define DERECHA 0
-#define IZQUIERDA 1
-#define PARADA 2
-#define ARRIBA 3
-#define ABAJO 4
+#define LIMITE_CERCA_X_PRESSED 0
+#define LIMITE_LEJOS_X_PRESSED 0
+#define LIMITE_CERCA_Y_PRESSED 0
+#define LIMITE_LEJOS_Y_PRESSED 0
 
-#define sizearray 100
+// Grados pinza, MAX y MIN admitido
+#define GMAX 110
+#define GMIN 10
 
-int Xder = 8;
-int Xizq = 13;
-
-int Yab = 7;
-int Yar = 11;
+#define AGARRE_PINZA 112
 
 Servo servoX;
-int pinServoX = 5;
-
 Servo servoY;
-int pinServoY = 5;
+Servo servoP;
 
-int posicion[sizearray];
-long tiempo[sizearray];
-int i;
+//GRANDE 82
+//GRANDE 112
 
-long tiempoInicial;
+int limiteLejosX = 13;
 
-int modo;
-int modoAnterior;
-int grabando;
-int reproduciendo;
+int limiteCercaX = 8;
+int limiteLejosY = 11;
+int limiteCercaY = 7;
+
+
+double tiempoCentimetroX = 0;
+double tiempoCentimetroY = 0;
+
+double posicionX = 0;
+double posicionY = 0;
 
 void setup() {
+  // inicializar pin 2 (Entrada)
+  pinMode(boton_pin, INPUT_PULLUP);
   Serial.begin(9600);
-  servoX.attach(pinServoX);
-  servoY.attach(pinServoY);
+  servoX.attach(5);
+  servoY.attach(3);
+  servoP.attach(6);
+  servoP.write(GMIN);
 
-  pinMode(Xder, INPUT);
-  pinMode(Yab, INPUT);
+  pinMode(limiteLejosX, INPUT);
+  pinMode(limiteCercaX, INPUT);
+  pinMode(limiteLejosY, INPUT);
+  pinMode(limiteCercaY, INPUT);
 
-  modo = PARADA;
-  grabando = 0;
-  reproduciendo = 0;
-  i = 0;
+  calibrarX();
+  calibrarY();
+
+  //ir inicio
+  irACentimetroX(0);
+  irACentimetroY(5);
+
+  //agarrar primer cubo
+  irACentimetroX(0);
+  servoP.write(GMIN);
+  irACentimetroY(24);
+  delay(750);
+  servoP.write(AGARRE_PINZA);
+  delay(500);
+  irACentimetroY(15);
+
+  //soltar primer cubo
+  irACentimetroX(20);
+  irACentimetroY(24);
+  delay(750);
+  servoP.write(GMIN);
+  delay(500);
+  irACentimetroY(15);
+
+  //agarrar segundo cubo
+  irACentimetroX(10);
+  irACentimetroY(24);
+  delay(750);
+  servoP.write(AGARRE_PINZA);
+  delay(500);
+  irACentimetroY(15);
+
+  //soltar segundo cubo
+  irACentimetroX(20);
+  irACentimetroY(22);
+  delay(750);
+  servoP.write(GMIN);
+  delay(500);
 }
-
+ 
 void loop() {
-  int valorX = analogRead(X_pinX);
-  int valorY = analogRead(Y_pinY);
-  //Serial.println("X: "+String(valorX));
-
-  if (valorX < 412) {
-    servoX.write(180);
-
-    if (grabando == 1 && modo != IZQUIERDA) {
-      modoAnterior = modo;
-      grabar();
-      tiempoInicial = millis();
-    }
-    modo = IZQUIERDA;
-
-  }
-  else if (valorX > 412 && valorX < 612 || valorY > 412 && valorY < 612) {
-    servoX.write(90);
-
-    if (grabando == 1 && modo != PARADA) {
-      modoAnterior = modo;
-      grabar();
-      tiempoInicial = millis();
-    }
-    modo = PARADA;
-  }
-  else if (valorX > 612) {
-    servoX.write(0);
-
-    if (grabando == 1 && modo != DERECHA) {
-      modoAnterior = modo;
-      grabar();
-      tiempoInicial = millis();
-    }
-    modo = DERECHA;
-
-  }
-  else if (valorY < 412) {
-    servoY.write(180);
-
-    if (grabando == 1 && modo != ARRIBA) {
-      modoAnterior = modo;
-      grabar();
-      tiempoInicial = millis();
-    }
-    modo = ARRIBA;
-
-  }
-  else if (valorY > 612) {
-    servoY.write(0);
-
-    if (grabando == 1 && modo != ABAJO) {
-      modoAnterior = modo;
-      grabar();
-      tiempoInicial = millis();
-    }
-    modo = ABAJO;
-
-  }
-
-  if (Serial.available() != 0) {
-
-    String comando = Serial.readString();
-
-    if (comando == "g") {
-      Serial.println("Grabando");
-      grabando = 1;
-      reproduciendo = 0;
-      i = 0;
-      irA0();
-      modo = PARADA;
-      tiempoInicial = millis();
-    }
-
-    if (comando == "r") {
-      Serial.println("Reproduciendo");
-      grabando = 0;
-      reproduciendo = 1;
-      irA0();
-    }
-
-  }
-
-  if (reproduciendo == 1) {
-    Serial.println("reproducion");
-    int j;
-    for (j = 0; j < i; j++) {
-      Serial.println("tiempo[j]:" + String(tiempo[j]) + ", posicion[j]: " + String(posicion[j]));
-      if (posicion[j] == DERECHA) {
-        servoX.write(0);
-        delay(tiempo[j]);
-      }
-      else if (posicion[j] == IZQUIERDA) {
-        servoX.write(180);
-        delay(tiempo[j]);
-      }
-      else if (posicion[j] == PARADA) {
-        servoX.write(90);
-        delay(tiempo[j]);
-      }
-      else if (posicion[j] == ARRIBA) {
-        servoY.write(180);
-        delay(tiempo[j]);
-      }
-      else if (posicion[j] == ABAJO) {
-        servoY.write(0);
-        delay(tiempo[j]);
-      }
-    }
-    reproduciendo = 0;
-  }
-
+  
 }
 
-void grabar() {
-  int tiempoFin = millis() - tiempoInicial;
-  posicion[i] = modoAnterior;
-  tiempo[i] = tiempoFin;
-  Serial.println("tiempoInicial: " + String(tiempoInicial) + ", tiempoFin:" + String(tiempoFin) + " tiempo[i]:" + String(tiempo[i]) + ", posicion[i]: " + String(posicion[i]));
-  i++;
-}
-
-void irA0() {
+void calibrarX(){
+  unsigned long tiempoInicio = 0;
+  unsigned long tiempoTotal = 0;
   servoX.write(0);
-  while (digitalRead(Xder) == HIGH) { }
+  while(true){
+    if(LIMITE_CERCA_X_PRESSED == digitalRead(limiteCercaX))
+      break;
+  }
+  servoX.write(180);
+  tiempoInicio = millis();
+  while(true){
+     if(LIMITE_LEJOS_X_PRESSED == digitalRead(limiteLejosX))
+        break;
+  }
   servoX.write(90);
-
-  servoY.write(0);
-  while (digitalRead(Yab) == HIGH) { }
-  servoY.write(90);
+  tiempoTotal = millis() - tiempoInicio;
+  tiempoCentimetroX = tiempoTotal/24;
 }
+
+
+void calibrarY(){
+  unsigned long tiempoInicio = 0;
+  unsigned long tiempoTotal = 0;
+  servoY.write(0);
+  while(true){
+    if(LIMITE_CERCA_Y_PRESSED == digitalRead(limiteCercaY))
+      break;
+  }
+  servoY.write(180);
+  tiempoInicio = millis();
+  while(true){
+     if(LIMITE_LEJOS_Y_PRESSED == digitalRead(limiteLejosY))
+        break;
+  }
+  servoY.write(90);
+  tiempoTotal = millis() - tiempoInicio;
+  tiempoCentimetroY = tiempoTotal/24;
+}
+
+
+
+void irACentimetroX(double centimetro){
+  double distancia = centimetro - posicionX;
+
+  if(distancia>0){
+    servoX.write(0);
+    delay(distancia*tiempoCentimetroX);
+    servoX.write(90);
+  }else if(distancia<0){
+    servoX.write(180);
+    delay(abs(distancia)*tiempoCentimetroX);
+    servoX.write(90);
+  }
+  posicionX=centimetro;
+}
+
+void irACentimetroY(double centimetro){
+  double distancia = centimetro - posicionY;
+
+  if(distancia>0){
+    servoY.write(0);
+    delay(distancia*tiempoCentimetroY);
+    servoY.write(90);
+  }else if(distancia<0){
+    servoY.write(180);
+    delay(abs(distancia)*tiempoCentimetroY);
+    servoY.write(90);
+  }
+  posicionY=centimetro;
+}
+
 
