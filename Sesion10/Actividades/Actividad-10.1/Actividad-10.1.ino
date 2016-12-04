e633fcea41fb43f8596bca666acd76b0440a9d8c
Santiago Martin <santiagomartinagra@gmail.com>
2016-12-04 18:54:49 +0100

    Todo ordenado

--- Sesion10/Actividades/Actividad-10.1/Actividad-10.1.ino
+++ Sesion10/Actividades/Actividad-10.1/Actividad-10.1.ino
@@ -3,52 +3,152 @@
 int NO_LINEA = 1;
 int LINEA = 0;
 
+int PIN_IZQ = 0;
+int PIN_C_IZQ = 1;
+int PIN_C_DER = 2;
+int PIN_DER = 3;
+
 Servo servoIzq;
 Servo servoDer;
 
 int pinServoDer = 9;
 int pinServoIzq = 8;
-int pinIrDer = 3;
-int pinIrIzq = 2;
 
+int pinIrDerCen = 3;
+int pinIrIzqCen = 2;
+int pinIrDer = 11;
+int pinIrIzq = 10;
+
+int pinesSensores[] = {10, 2, 3, 11};
+int sensor[4];
+
+int compensacion = 10;
+
+int HORARIO = 110;
+int ANTIHORARIO = 70;
+
+int PARAR = 90;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode( pinIrDerCen , INPUT);
+  pinMode( pinIrIzqCen , INPUT);
   pinMode( pinIrDer , INPUT);
   pinMode( pinIrIzq , INPUT);
 
   servoIzq.attach(pinServoIzq);
   servoDer.attach(pinServoDer);
-
-
+  //delay(10000);
 }
 
 void loop() {
-  int irDer = digitalRead(pinIrDer);
-  int irIzq = digitalRead(pinIrIzq);
 
-  //horario -> 0
-  //antihorario -> 180
-  
-  //Adelante
-  if (irDer == LINEA && irIzq == LINEA){
-    servoIzq.write(0); 
-    servoDer.write(180);
+  actualizarTodosLosSensores();
+
+  if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == LINEA
+      && sensor[PIN_IZQ] == NO_LINEA && sensor[PIN_DER] == NO_LINEA) {
+    servoIzq.write(ANTIHORARIO - compensacion);
+    servoDer.write(HORARIO);
   }
-  //Izquierda
-  else if (irDer != LINEA && irIzq == LINEA){
-    servoIzq.write(90); 
-    servoDer.write(50);
+  else if (sensor[PIN_C_IZQ] == NO_LINEA && sensor[PIN_C_DER] == LINEA
+           && sensor[PIN_IZQ] == NO_LINEA && sensor[PIN_DER] == NO_LINEA) {
+    servoIzq.write(HORARIO - compensacion);
+    servoDer.write(HORARIO);
   }
-  //Derecha
-  else if (irDer == LINEA && irIzq != LINEA){
-    servoIzq.write(140); 
-    servoDer.write(90);
+  else if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == NO_LINEA
+           && sensor[PIN_IZQ] == NO_LINEA && sensor[PIN_DER] == NO_LINEA) {
+    servoIzq.write(ANTIHORARIO - compensacion);
+    servoDer.write(ANTIHORARIO);
   }
-  //Atras
-  else if (irDer != LINEA && irIzq != LINEA){
-    servoIzq.write(90); 
-    servoDer.write(90);
+  else if (sensor[PIN_C_IZQ] == NO_LINEA && sensor[PIN_C_DER] == NO_LINEA
+           && sensor[PIN_IZQ] == NO_LINEA && sensor[PIN_DER] == NO_LINEA) {
+    servoIzq.write(ANTIHORARIO);
+    servoDer.write(ANTIHORARIO);
+  }
+  else if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == LINEA
+           && sensor[PIN_IZQ] == LINEA && sensor[PIN_DER] == NO_LINEA) {
+    delay(500);
+    girarIzquierda();
+  }
+  else if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == LINEA
+           && sensor[PIN_IZQ] == NO_LINEA && sensor[PIN_DER] == LINEA) {
+    delay(500);
+    girarDerecha();
+  }
+  else if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == LINEA
+           && sensor[PIN_IZQ] == LINEA && sensor[PIN_DER] == LINEA) {
+    delay(500);
+    actualizarTodosLosSensores();
+    if (sensor[PIN_C_IZQ] == LINEA && sensor[PIN_C_DER] == LINEA
+        && sensor[PIN_IZQ] == LINEA && sensor[PIN_DER] == LINEA) {
+      servoIzq.write(PARAR);
+      servoDer.write(PARAR);
+    }
+    else {
+      girarDerecha();
+    }
   }
 
+}
 
+
+void actualizarTodosLosSensores() {
+  for (int i = 0; i < 4; i++) {
+    sensor[i] =  digitalRead(pinesSensores[i]);
+  }
+
+  // 4 lecturas adicionales
+  // Se guardan las que detectan línea en cualquiera de los 4 barridos
+  for (int i = 0; i < 4; i++) {
+    delay(20); // Delay con el movimiento actual
+
+    for (int i = 0; i < 4; i++) {
+      if (digitalRead(pinesSensores[i]) == LINEA)
+        sensor[i] = LINEA;
+    }
+  }
+}
+
+void girarDerecha() {
+  // girar hacia la derecha
+  servoDer.write(ANTIHORARIO);
+  servoIzq.write(ANTIHORARIO);
+  delay(400); // Pequeño delay para que le dé tiempo a salir de su carril actual
+  // Si no puede que este ya en un carril y detectaría línea
+
+  boolean carrilEncontrado = false;
+  while (!carrilEncontrado) {
+    int Sensor2 = digitalRead(pinIrIzqCen);
+    int Sensor3 = digitalRead(pinIrDerCen);
+
+    if ( Sensor2 == LINEA && Sensor3 == LINEA ) {
+      carrilEncontrado = true;
+    }
+  }
+
+  // Parar
+  servoIzq.write(ANTIHORARIO - compensacion);
+  servoDer.write(HORARIO);
+}
+
+void girarIzquierda() {
+  // girar hacia la derecha
+  servoDer.write(HORARIO);
+  servoIzq.write(HORARIO);
+  delay(400); // Pequeño delay para que le dé tiempo a salir de su carril actual
+  // Si no puede que este ya en un carril y detectaría línea
+
+  boolean carrilEncontrado = false;
+  while (!carrilEncontrado) {
+    int Sensor2 = digitalRead(pinIrIzqCen);
+    int Sensor3 = digitalRead(pinIrDerCen);
+
+    if ( Sensor2 == LINEA && Sensor3 == LINEA ) {
+      carrilEncontrado = true;
+    }
+  }
+
+  // Parar
+  servoIzq.write(ANTIHORARIO - compensacion);
+  servoDer.write(HORARIO);
 }
--- Sesion9/Guion/Guion-9.1/Guion-9.1.ino
+++ Sesion9/Guion/Guion-9.1/Guion-9.1.ino
@@ -1,76 +1,37 @@
-#include <SPI.h>
-#include <Ethernet.h>
+#include <Servo.h>
 
-// IMPORTANTE!!!!!CADA UNO QUE PONGA UNA MAC 
-// ultimo digito segun su grupo 0x21 - 0x22 , 0x22
-byte mac[] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};  
-IPAddress dnServer(156, 35, 14 , 2);
-IPAddress gateway(192, 168, 61, 13);
-IPAddress subnet(255, 255, 255, 0);
-// IMPORTANTE!!!!!CADA UNO QUE PONGA UNA IP
-// ultimo digito según su grupo 51, 52, 53, 54
-IPAddress ip(192, 168, 61, 250);
+Servo servoIzq;
+Servo servoDer;
 
-char server[] = "www.mderg.com";
-EthernetClient client;
+int pinServoIzq = 8;
+int pinServoDer = 9;
 
-int pin_sensor = 7;
-int ultimoValor = -1;
-long ultimoValorTime = 0;
-String identificadorPlaza = "666";
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(pin_sensor, INPUT);
-  Ethernet.begin(mac, ip, dnServer, gateway, subnet);
-  Serial.println(Ethernet.localIP());
-  delay(1000);
+void setup()
+{
+  servoIzq.attach(pinServoIzq);
+  servoDer.attach(pinServoDer);
 }
-
 
 void loop() {
-  int valorActual = digitalRead(pin_sensor);
+    // Atras
+  servoIzq.write(180); // Vel Máxima
+  servoDer.write(0);
+  delay(3000);
+  servoIzq.write(100); // Próximo a 90, despacio
+  servoDer.write(80);
+  delay(3000);
   
-  if (millis() - ultimoValorTime > 3000 && valorActual != ultimoValor){
-     Serial.println("Enviar peticion valor="+String(valorActual));
-actualizar(identificadorPlaza, String(valorActual));
-     
-     ultimoValor = valorActual; // Actualizar ultimo valor
-     ultimoValorTime = millis();
-  }
-}
+  servoIzq.write(90); // Detener
+  servoDer.write(90);
+  delay(3000);
+  
+// Alante
+  servoIzq.write(80); // Próximo a 90, despacio
+  servoDer.write(100);
+  delay(3000);
+  servoIzq.write(0); // Vel Máxima
+  servoDer.write(180);
+  delay(3000);
 
-String respuesta = "";
-void actualizar(String idPlaza,String estadoPlaza){
-  if (client.connect(server, 80)) {
-    Serial.println("conectado");
-    
-    client.println("GET /robotica/parking13/actualizarplazag.php?i="
-+idPlaza+"&o="+estadoPlaza+" HTTP/1.1");
-    client.println("Host: www.mderg.com");
-    client.println("Connection: close");
-    client.println();
-    
-    delay(3000); // Esperar por seguridad
-    
-    while (client.available()) {
-        char c = client.read();
-        respuesta+=c;
-    } 
-    if (!client.connected()) {
-        Serial.println("respuesta -------------------------------");
-        Serial.println(respuesta);
-        Serial.println("Desconectado ---------------------------");
-        client.stop();
-    }
-    
-  } else {
-    Serial.println("Fallo de conexión");
-     // RESET a veces falla y hay que reiniciarlo
-     client.stop();
-     delay(1000);
-     Ethernet. begin(mac, ip, dnServer, gateway, subnet);
-     delay(1000);
-   }
-}
 
+}
